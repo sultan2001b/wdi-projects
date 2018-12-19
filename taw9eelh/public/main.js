a45880473f3fe760c1a0d3755a3f8d96edefe8eb
Abdullah-AlQahtani <abdullahq.ksa@gmail.com>
2018-12-19 19:25:26 +0300

    Fix Some CSS

--- taw9eelh/public/main.js
+++ taw9eelh/public/main.js
@@ -1,30 +1,9 @@
 $(document).ready( function () {
     
-    function showPosition(position) {
-        x.innerHTML = "Latitude: " + position.coords.latitude + 
-        "Longitude: " + position.coords.longitude; 
-      }
-
     $('#table_id').DataTable();
     $('#table_id2').DataTable();
     $('#date').datepicker({
         uiLibrary: 'bootstrap4'
-    });
-    $("#fromCheckbox").click(function () {
-        if ($(this).prop("checked")) {
-            $("#from").val("jQuery");
-        }
-        else {
-            $("#from").val("");
-        }
-    });
-    $("#toCheckbox").click(function () {
-        if ($(this).prop("checked")) {
-            $("#to").val(navigator.geolocation.getCurrentPosition(showPosition));
-        }
-        else {
-            $("#to").val("");
-        }
     });
 } );
 
